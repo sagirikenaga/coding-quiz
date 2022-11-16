adbe2da54dfb765724607f117fab036975389793
Sagiri <sagiri.ikenaga@ryerson.ca>
2022-11-16 00:14:15 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,29 @@
-
+//global variables
 var questionContainer = document.querySelector('.question-container')
-
 var scoreModalEl = document.getElementById("highscore-modal");
 var scoreCloseEl = document.querySelector(".score-modal-close");
 var scoreButtonEl = document.getElementById("highscore");
 var highscoresListEl = document.querySelector("#high-scores-list");
-
 var timeEl = document.querySelector("#timer");
 var gameOverModal = document.getElementById("time-modal")
-
 var startBtnEl = document.querySelector("#start-btn");
-var scoresCount = document.querySelector("#scores-count")
-var highScoreDisplay = document.querySelector('#high-scores-count');
-
 var playAgainBtnEl = document.querySelector("#play-again-btn")
 var winPlayAgainBtn = document.querySelector("#win-play-again")
-var winModalEl = document.querySelector("#win-modal")
-
-var timer;
-var secondsLeft = 120;
-
 var questionEl = document.querySelector("#question-display");
 var answersEl = document.querySelector("answers-display");
 var answerButtons = document.getElementsByClassName("answer-btn")
 var questionDisplayEl = document.querySelector("#question-display");
 var answerBoxEl = document.querySelector("#answer-box");
 let submitScore = document.querySelector("#submit-btn");
-
 let initialsEl = document.querySelector("#initials");
-let storedList = JSON.parse(localStorage.getItem("highscores")) || [];
-let gameIndex = 0;
 let formEl = document.querySelector("#form")
+let storedList = JSON.parse(localStorage.getItem("highscores")) || [];
 
+let gameIndex = 0;
+var timer;
+var secondsLeft = 120;
+
+//list of questions and answers
 var questionList = [
     {
         question: 'Commonly used data types do NOT include:',
@@ -80,6 +72,8 @@ var questionList = [
     }
 ]
 
+//function to loop through questionList array and display the question and corresponding answers to each button
+// assigns onclick to buttons 
 function displayQuestion(i) {
     console.log(i);
     if (i >= 5) {
@@ -98,10 +92,8 @@ function displayQuestion(i) {
     })
 }
 
+//function that checks if clicked answer button is correct; red if wrong and deducts 10 seconds from timer, green if true and runs next question
 function checkAnswer(event) {
-    console.log(this);
-    console.log(event.target)
-    // button.addEventListener('click', () => {
         if (this.value === 'true') {
             this.style.color = "green";
             displayQuestion(gameIndex+=1);
@@ -109,17 +101,17 @@ function checkAnswer(event) {
             this.style.color = "red";
             secondsLeft -= 10;
         }
-    // }, {once:true});
 }
 
+//function that runs when timer is up -> clears interval, displays game over and displays modal to enter initials and play again
 function gameOver() {
     clearInterval(timer);
     questionEl.innerText = "GAME OVER";
     answerBoxEl.style.display="none";
     gameOverModal.style.display="block";
-    // storeScores();
 }
 
+// adds event listener to submit button when initials are submitted -> sets into local storage
 submitScore.addEventListener('click', function (event) {
     event.preventDefault();
     var newScore = {
@@ -132,17 +124,17 @@ submitScore.addEventListener('click', function (event) {
     renderScores();
 })
 
+// displays the scores from local storage onto high score modal
 function renderScores() {
     for (var i=0; i < 5; i++) {
         var li = document.createElement("li");
         var reverse = storedList.reverse();
-        console.log(reverse)
         li.textContent = reverse[i].initials + ": " + reverse[i].time;
         highscoresListEl.appendChild(li);
     }
 }
 
-//initializes all start game functions
+// function to start the game 
 function startGame() {
     console.log("Game started!");
     isGameOver = false; 
@@ -164,8 +156,10 @@ function startTimer() {
     }, 1000);
 }
 
+// event listener to run function display module when highscore button is clicked 
 scoreButtonEl.addEventListener ('click', showScoreModal);
 
+//displays the highscore modal
 function showScoreModal() {
     scoreModalEl.style.display="block";
 }
@@ -175,11 +169,11 @@ scoreCloseEl.addEventListener('click',function() {
     scoreModalEl.style.display="none";
 });
 
+// reloads the page when user wants to play again
 playAgainBtnEl.addEventListener('click', function() {
     window.location.reload();
 })
 
-
-//starts game
+//starts game when button is clicked
 startBtnEl.addEventListener('click', startGame)
 renderScores();
